0a5afef363ae146e00b389bc94ebea5ee59bfb79
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-05-05 17:23:02 -0400

    Remove Unused Default Props

--- src/js/default_props.js
+++ src/js/default_props.js
@@ -1,14 +1,6 @@
 const defaultProps = {
   // Sizes
   xs: 12,
-  sm: undefined,
-  md: undefined,
-  lg: undefined,
-  // Offsets
-  xsOffset: undefined,
-  smOffset: undefined,
-  mdOffset: undefined,
-  lgOffset: undefined,
   // Block changes inputs with layout: "horizontal" to use the full width of
   // their container and disables the label.
   block: false,
