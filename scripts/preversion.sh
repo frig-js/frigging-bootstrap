52e5cce23b460d1b52463e9151e8952cdab402d0
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-05-18 18:14:49 -0400

    Remove Minified Build Option In Preversion ...
    Minified output is no longer being referenced in package.json as
    the main entry point, we use the non-minified version now.

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -7,8 +7,3 @@ echo ""
 echo "FRIGGING BOOTSTRAP: COMPILING DEV RELEASE"
 echo "======================================================="
 FRIG_ENV="production" webpack --progress --bail
-
-echo ""
-echo "FRIGGING BOOTSTRAP: COMPILING MINIFIED RELEASE"
-echo "======================================================="
-FRIG_ENV="production" FRIG_MIN="minify" webpack --progress --bail
