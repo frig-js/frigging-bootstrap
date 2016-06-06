0d10925cc0dce413aaaad953f0587f0940b939a0
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-06-06 18:06:41 -0400

    Move Value Link Select Code From Frig Core
    Move the Value Linked Select code from Frig Core to Frigging-Bootstrap's
    select because Value Link is no longer put of React. Also, Value Link
    was unable to access the value in React DOM's select, Value Linked
    Select fixed this. With the removal of Value Link, from React, having
    this fix is no longer needed.
    
    This change will also allow Frig theme developers to create themes with
    their own version of select and not have to use the default select in
    Frig core allow for a smaller code base in the core.

--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,7 +1,6 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import cx from 'classnames'
-
-import { ValueLinkedSelect } from 'frig'
 
 import InputErrorList from './input_error_list'
 import Saved from './saved'
@@ -14,27 +13,78 @@ export default class Select extends React.Component {
   static displayName = 'FriggingBootstrap.Select'
 
   static defaultProps = Object.assign({}, defaultProps, {
-    options: {},
+    options: [],
   })
 
   static propTypes = Object.assign({},
     defaultPropTypes, {
-      options: React.PropTypes.oneOfType([
-        React.PropTypes.array,
-        React.PropTypes.object,
-        React.PropTypes.string,
-      ]),
+      options: React.PropTypes.array,
     }
   )
+
+  componentWillMount() {
+    const hasOptions = this.props.options.length !== 0
+    if (hasOptions && this.props.value === '') {
+      this._setInitialValue(this.props)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const hasOptions = nextProps.options.length !== 0
+    // Setting the intial value of the select when the options load
+    if (hasOptions && nextProps.value === '') {
+      this._setInitialValue(nextProps)
+    }
+    // Blanking the select's value when the options are removed
+    if (!hasOptions && nextProps.value !== '') {
+      nextProps.onChange(undefined, { setModified: false })
+    }
+  }
+
+  // Default a blank value to the first option (if there are no blank options)
+  _setInitialValue(nextProps) {
+    if (nextProps.options.filter(({ value }) => value === '').length > 0) {
+      return
+    }
+    const value = nextProps.value || nextProps.options[0].value
+    nextProps.onChange(value, { setModified: false })
+  }
+
+  // Reads the value from the DOM for the select input fields
+  _getValue() {
+    const el = ReactDOM.findDOMNode(this.refs.input)
+    // The value is cast to a string when we get it from DOM.value. This is a
+    // mapping of those strings to their original values in the options list.
+    const originalValues = {}
+    for (const option of this.props.options) {
+      const valueHash = option.value.toString()
+      originalValues[valueHash] = option.value
+    }
+
+    return originalValues[el.value] || el.value
+  }
+
+  _onChange() {
+    this.props.onChange(this._getValue())
+  }
 
   _inputHtml() {
     return Object.assign({}, this.props.inputHtml, {
       key: 'input',
+      ref: 'input',
       className: 'form-control',
       value: this.props.value,
       onChange: this.props.onChange,
       options: this.props.options,
     })
+  }
+
+  _selectOption(o) {
+    const attrs = {
+      key: `option-${o.label}`,
+      value: o.value || '',
+    }
+    return <option {...attrs}>{o.label}</option>
   }
 
   render() {
@@ -44,7 +94,9 @@ export default class Select extends React.Component {
         <div className={formGroupCx(this.props)}>
           <Label {...labelProps} />
           <div className="controls">
-            <ValueLinkedSelect {...this._inputHtml()} />
+            <select {...this._inputHtml()}>
+              {this.props.options.map(this._selectOption)}
+            </select>
             <InputErrorList errors={this.props.errors} />
           </div>
           <Saved saved={this.props.saved} />
--- test/components/select.test.js
+++ test/components/select.test.js
@@ -4,15 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
-import { ValueLinkedSelect } from 'frig'
-
 import Select from '../../src/js/components/select'
 import InputErrorList from '../../src/js/components/input_error_list'
 import Saved from '../../src/js/components/saved'
 import Label from '../../src/js/components/label'
+import td from 'testdouble'
 
-
-const props = {
+const defaultProps = {
   value: 'red',
   onChange: () => {},
   inputHtml: {},
@@ -25,40 +23,189 @@ const props = {
 
 describe('<Select />', () => {
   it('renders', () => {
-    mount(<Select {...props} />)
+    mount(<Select {...defaultProps} />)
   })
 
-  it('renders ValueLinkedSelect tag with attrs from props.inputHtml', () => {
-    const inputHtml = { disabled: true }
-    const wrapper = mount(<Select {...props} inputHtml={inputHtml} />)
-    const select = wrapper.find(ValueLinkedSelect)
-    expect(select.prop('disabled')).to.be.true()
+  describe('when using default props', () => {
+    it('renders Select tag with attrs from props.inputHtml', () => {
+      const inputHtml = { disabled: true }
+      const wrapper = mount(<Select {...defaultProps} inputHtml={inputHtml} />)
+      const select = wrapper.find('select')
+      expect(select.prop('disabled')).to.be.true()
+    })
+
+    it('renders select tag with props.value as selected value', () => {
+      const wrapper = mount(<Select {...defaultProps} />)
+      const select = wrapper.find('select')
+      const node = ReactDOM.findDOMNode(select.get(0))
+      expect(node.value).to.equal('red')
+    })
+
+    it('renders InputErrorList with props.errors', () => {
+      const errors = ['should be something', 'should be something else']
+      const wrapper = mount(<Select {...defaultProps} errors={errors} />)
+      const inputErrorList = wrapper.find(InputErrorList)
+      expect(inputErrorList.prop('errors')).to.equal(errors)
+    })
+
+    it('renders Saved with defaultProps.saved', () => {
+      const wrapper = mount(<Select {...defaultProps} saved />)
+      const saved = wrapper.find(Saved)
+      expect(saved.prop('saved')).to.be.true()
+    })
+
+    it('renders Label with defaultProps.label', () => {
+      const labelText = 'Pick an option'
+      const wrapper = mount(<Select {...defaultProps} label={labelText} />)
+      const label = wrapper.find(Label)
+      expect(label.prop('label')).to.equal('Pick an option')
+    })
   })
 
-  it('renders select tag with props.value as selected value', () => {
-    const wrapper = mount(<Select {...props} />)
-    const select = wrapper.find('select')
-    const node = ReactDOM.findDOMNode(select.get(0))
-    expect(node.value).to.equal('red')
-  })
+  describe('checking the different options types', () => {
+    const noop = () => {}
 
-  it('renders InputErrorList with props.errors', () => {
-    const errors = ['should be something', 'should be something else']
-    const wrapper = mount(<Select {...props} errors={errors} />)
-    const inputErrorList = wrapper.find(InputErrorList)
-    expect(inputErrorList.prop('errors')).to.equal(errors)
-  })
+    describe('_getValue', () => {
+      const testGetValue = (options, value, onChange, expected) => {
+        const props = { options, value, onChange }
+        const wrapper = mount(<Select { ...props } />)
+        const instance = wrapper.instance()
+        expect(instance._getValue()).to.equal(expected)
+      }
 
-  it('renders Saved with props.saved', () => {
-    const wrapper = mount(<Select {...props} saved />)
-    const saved = wrapper.find(Saved)
-    expect(saved.prop('saved')).to.be.true()
-  })
+      it('should return the value of the selected option', () => {
+        const options = [
+          { value: 'CA', label: 'Canada' },
+          { value: 'US', label: 'United States' },
+        ]
+        testGetValue(options, 'US', noop, 'US')
+      })
 
-  it('renders Label with props.label', () => {
-    const labelText = 'Pick an option'
-    const wrapper = mount(<Select {...props} label={labelText} />)
-    const label = wrapper.find(Label)
-    expect(label.prop('label')).to.equal('Pick an option')
+      it('should handle numeric values as Number type', () => {
+        const options = [
+          { value: 1, label: 'Canada' },
+          { value: 2, label: 'United States' },
+        ]
+        testGetValue(options, 2, noop, 2)
+      })
+
+      it('should return \'\' when value is blank', () => {
+        const options = [
+          { value: '', label: '' },
+          { value: 'US', label: 'United States' },
+        ]
+        testGetValue(options, '', noop, '')
+      })
+    })
+
+    describe('render', () => {
+      it('when props.options is empty, renders select with no options', () => {
+        const wrapper = mount(<Select value={''} onChange={noop} />)
+        const options = wrapper.find('option')
+        expect(options).to.have.lengthOf(0)
+      })
+
+      it('when props.options exists, renders select with options', () => {
+        const props = {
+          options: [
+            { value: 'CA', label: 'Canada' },
+            { value: 'US', label: 'United States' },
+          ],
+          value: '',
+          onChange: noop,
+        }
+        const wrapper = mount(<Select {...props} />)
+        const options = wrapper.find('option')
+        expect(options).to.have.lengthOf(2)
+      })
+
+      it('renders an input with onChange that calls Select.props.onChange', () => {
+        const onChange = td.function.call()
+        const props = {
+          options: [
+            { value: 'CA', label: 'Canada' },
+            { value: 'US', label: 'United States' },
+          ],
+          value: 'US',
+          onChange,
+        }
+        const wrapper = mount(<Select {...props} />)
+
+        const select = wrapper.find('select')
+        const selectOnChange = select.prop('onChange')
+
+        // requestChange gets called on component mount, clear the slate
+        td.reset()
+
+        // we want to verify that Select's props.requestChange
+        // is the same function passed to <select>'s valuelink. Args don't
+        // matter. (They won't be exactly the same since the onChange handler
+        // goes through _getValue, rather than respecting the argument passed
+        // to requestChange)
+        selectOnChange()
+        td.verify(onChange(), { ignoreExtraArgs: true })
+      })
+    })
+
+    describe('componentWillMount / componentDidReceiveProps', () => {
+      describe('when value=\'\' (e.g. no selection)', () => {
+        it('onChange is called with value of 1st option', () => {
+          const onChange = td.function.call()
+          const props = {
+            options: [
+              { value: 'CA', label: 'Canada' },
+              { value: 'US', label: 'United States' },
+            ],
+            value: '',
+            onChange,
+          }
+
+          // requestChange should be called on component mount
+          const wrapper = mount(<Select { ...props } />)
+          td.verify(onChange('CA', { setModified: false }), { times: 1 })
+
+          // simulate a re-render of the component with the same props
+          wrapper.setProps(props)
+          td.verify(onChange('CA', { setModified: false }), { times: 2 })
+        })
+
+        it('and ANY options value is \'\', requestChange not called', () => {
+          const onChange = td.function()
+          const props = {
+            options: [
+              { value: '', label: '-- Select --' },
+              { value: 'CA', label: 'Canada' },
+              { value: 'US', label: 'United States' },
+            ],
+            value: '',
+            onChange,
+          }
+
+          mount(<Select { ...props } />)
+
+          td.verify(onChange(), { times: 0, ignoreExtraArgs: true })
+        })
+      })
+
+      describe('options went from 1 to 0', () => {
+        it('calls requestChange(undefined) on re-render', () => {
+          const onChange = td.function.call()
+          const props = {
+            options: [
+              { value: 'CA', label: 'Canada' },
+            ],
+            value: 'CA',
+            onChange,
+          }
+
+          const wrapper = mount(<Select { ...props } />)
+
+          props.options = []
+          wrapper.setProps(props)
+
+          td.verify(onChange(undefined), { ignoreExtraArgs: true })
+        })
+      })
+    })
   })
 })
